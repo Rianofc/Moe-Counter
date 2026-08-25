39127fc43e5dfd828672bea3177d2938103d143a
SeptiaDev <142219160+SeptiaDev@users.noreply.github.com>
2026-08-25 08:08:13 +0800

    Update mongodb.js

--- db/mongodb.js
+++ db/mongodb.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose')
 const schema = require('./schema')
 
 // the default mongodb url (local server)
-const mongodbURL = process.env.DB_URL || 'mongodb+srv://mcariawan:api2352@cluster0.whnuqrl.mongodb.net/?retryWrites=true&w=majority'
-
+const mongodbURL = process.env.DB_URL || ''
 mongoose.connect(mongodbURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
